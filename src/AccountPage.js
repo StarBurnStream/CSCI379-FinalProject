9ace11768163e9b81fe1e9edba7fe1bf8485aba8
hs031@bucknell.edu <hs031@bucknell.edu>
2018-04-29 17:18:17 -0400

    Haipu 04/29/2018

--- src/AccountPage.js
+++ src/AccountPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, NavItem, MenuItem, NavDropdown, Button, FormGroup, FormControl, Carousel } from 'react-bootstrap';
-import {Tab, Col, Row, Panel, ControlLabel, HelpBlock} from 'react-bootstrap';
+import {Tab, Col, Row, Panel, ControlLabel, HelpBlock, InputGroup, Radio} from 'react-bootstrap';
 import NavBar from "./NavBar";
 import { Route } from 'react-router-dom';
 
-class Mainpage extends Component {
+class AccountPage extends Component {
+
 	function(){
 	  document.getElementById('.carousel-showmanymoveone .item').each(function(){
 	    var itemToClone = document.getElementById(this);
@@ -25,25 +26,32 @@ class Mainpage extends Component {
 	  });
 	};
 
+	handleUpload = event => {
+		console.log(document.getElementById("ItemName").value);
+		console.log(document.getElementById("ItemPrice").value);
+		console.log(document.getElementById("ItemQuality").value);
+		console.log(document.getElementById("ItemDescription").value);
+		console.log(document.getElementById("ItemMethod").value);
+	}
 
 	render(){
 	  return (
 		<div>
 
       <div>
-        <div>
-          <img width={100} height={100} display='block' src="unicorn.jpg" />
-        </div>
+
         <div>
           <Tab.Container id="left-tabs-example" defaultActiveKey="first">
             <Row className="clearfix">
               <Col sm={2}>
                 <Nav bsStyle="pills" stacked>
+									<NavItem><img width={100} height={100} display='block' src="unicorn.jpg" /></NavItem>
                   <NavItem eventKey="Account Info">Account Info</NavItem>
                   <NavItem eventKey="Account Management">Account Management</NavItem>
                   <NavItem eventKey="Buy History">Buy History</NavItem>
                   <NavItem eventKey="Sell History">Sell History</NavItem>
                   <NavItem eventKey="Pending Order">Pending Order</NavItem>
+									<NavItem eventKey="Post Item">Post Item</NavItem>
                 </Nav>
               </Col>
               <Col sm={8}>
@@ -142,10 +150,56 @@ class Mainpage extends Component {
                     </Panel>
                   </Tab.Pane>
 
-                  <Tab.Pane eventKey="Buy History">Tab 2 conten</Tab.Pane>
+                  <Tab.Pane eventKey="Buy History">Tab 2 content</Tab.Pane>
                   <Tab.Pane eventKey="Sell History">Tab 2 content</Tab.Pane>
-                  <Tab.Pane eventKey="Pending History">Tab 2 content</Tab.Pane>
-                  <Tab.Pane eventKey="Post Item">Tab 2 content</Tab.Pane>
+                  <Tab.Pane eventKey="Pending Order">Tab content</Tab.Pane>
+
+                  <Tab.Pane eventKey="Post Item">
+
+										<Panel bsStyle="info">
+										<Panel.Heading>
+											<Panel.Title componentClass="h3">Item Information</Panel.Title>
+										</Panel.Heading>
+										<Panel.Body>
+											<FormGroup controlId="ItemName">
+												<p>Name of Item</p>
+												<FormControl type="text" placeholder="Name" />
+											</FormGroup>{' '}
+
+											<FormGroup controlId="ItemPrice">
+												<p>Item Price</p>
+												<InputGroup>
+												<InputGroup.Addon>$</InputGroup.Addon>
+												<FormControl type="integer" placeholder="Price" />
+												</InputGroup>
+											</FormGroup>{' '}
+
+											<FormGroup controlId="ItemQuality">
+												<p>Quality of Item</p>
+												<FormControl componentClass="select" placeholder="Quality">
+													<option defaultSelected value="New">New</option>
+													<option value="Like New">Like New</option>
+													<option value="Very Good">Very Good</option>
+													<option value="Good">Good</option>
+													<option value="Acceptable">Acceptable</option>
+												</FormControl>
+											</FormGroup>
+
+											<FormGroup controlId="ItemDescription">
+												<p>Item Description</p>
+												<FormControl componentClass="textarea" placeholder="Description" />
+											</FormGroup>{' '}
+
+											<FormGroup controlId="ItemMethod">
+												<p>Trade Method</p>
+												<FormControl type="text" placeholder="e.g. Cash or Venmo" />
+											</FormGroup>{' '}
+
+											<Button onClick={this.handleUpload}>Upload</Button>
+										</Panel.Body>
+										</Panel>
+									</Tab.Pane>
+
                 </Tab.Content>
               </Col>
             </Row>
@@ -159,4 +213,4 @@ class Mainpage extends Component {
 	}
 }
 
-export default Mainpage
+export default AccountPage
